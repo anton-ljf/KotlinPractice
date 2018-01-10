e69c38e8cb1794b7b84a1a9cf3db8e00b108a8c4
刘吉峰 <522905974@qq.com>
2018-01-10 19:43:17 +0800

    no message

---
+++ app/src/main/java/com/kotlinpractice/utils/Preference.kt
@@ -0,0 +1,120 @@
+package com.kotlinpractice.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.kotlinpractice.MyApplication
+import java.io.*
+import kotlin.reflect.KProperty
+
+/**
+ * Created by jifeng.liu on 18/1/10.
+ */
+class Preference<T>(val name:String,private val default:T) {
+    companion object {
+        private val fileName="kotlin_practice"
+    }
+
+    private val prefs: SharedPreferences by lazy{
+        MyApplication.context.getSharedPreferences(fileName, Context.MODE_PRIVATE)
+    }
+
+    operator fun getValue(thisRef:Any? ,property:KProperty<*>):T{
+        getSharedPreferences(name,default)
+    }
+
+    operator fun setValue(thisRef: Any?,property: KProperty<*>,value:T){
+        putSharedPreferences(name,default)
+    }
+
+    private fun putSharedPreferences(name :String ,value: T)= with(prefs.edit()){
+        when(value){
+            is Long -> putLong(name,value)
+            is String -> putString(name,value)
+            is Boolean -> putBoolean(name,value)
+            is Float -> putFloat(name,value)
+            is Int -> putInt(name,value)
+            else -> putString(name,serialize(value))
+        }.apply()
+    }
+
+
+    private fun getSharedPreferences(name:String,default: T):T = with(prefs){
+        val res:Any = when(default){
+            is Long -> getLong(name,default)
+            is Int -> getInt(name,default)
+            is String ->getString(name,default)
+            is Float -> getFloat(name,default)
+            is Boolean -> getBoolean(name,default)
+            else -> deSerialization(getString(name,serialize(default)))
+        }
+        return res as T
+    }
+
+    /**
+     * 反序列化对象
+
+     * @param str
+     * *
+     * @return
+     * *
+     * @throws IOException
+     * *
+     * @throws ClassNotFoundException
+     */
+    @Suppress("UNCHECKED_CAST")
+    @Throws(IOException::class, ClassNotFoundException::class)
+    private fun<A> deSerialization(str: String): A {
+        val redStr = java.net.URLDecoder.decode(str, "UTF-8")
+        val byteArrayInputStream = ByteArrayInputStream(
+                redStr.toByteArray(charset("ISO-8859-1")))
+        val objectInputStream = ObjectInputStream(
+                byteArrayInputStream)
+        val obj = objectInputStream.readObject() as A
+        objectInputStream.close()
+        byteArrayInputStream.close()
+        return obj
+    }
+
+    /**
+     * 序列化对象
+
+     * @param person
+     * *
+     * @return
+     * *
+     * @throws IOException
+     */
+    @Throws(IOException::class)
+    private fun<A> serialize(obj: A): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        val objectOutputStream = ObjectOutputStream(
+                byteArrayOutputStream)
+        objectOutputStream.writeObject(obj)
+        var serStr = byteArrayOutputStream.toString("ISO-8859-1")
+        serStr = java.net.URLEncoder.encode(serStr, "UTF-8")
+        objectOutputStream.close()
+        byteArrayOutputStream.close()
+        return serStr
+    }
+
+    /**
+     * 返回所有的键值对
+     *
+     * @param context
+     * @return
+     */
+    fun getAll(): Map<String, *> {
+        return prefs.all
+    }
+
+    /**
+     * 查询某个key是否已经存在
+     *
+     * @param key
+     * @return
+     */
+    fun contains(key: String): Boolean {
+        return prefs.contains(key)
+    }
+
+}
